e496c4a5efa3f1570792b4f8932ac0fdf028cddb
Compeee <eero.kaarnalehto@outlook.com>
2022-10-24 14:10:17 +0300

    backend progress

--- project/server/src/main/java/com/example/libraryapp/user/UserController.java
+++ project/server/src/main/java/com/example/libraryapp/user/UserController.java
@@ -1,5 +1,27 @@
 package com.example.libraryapp.user;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/users")
 public class UserController {
 
+    private static final List<User> USERS = Arrays.asList(
+            new User(1, "Joe Bond"),
+            new User(2, "Jane Doe"),
+            new User(3, "Maria Jones")
+    );
+
+    @GetMapping(path = "{userId}")
+    public User getUser(@PathVariable("userId") Integer userId){
+        return USERS.stream().filter(user -> userId.equals(user.getUserId()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("User" + userId + "doesn't exist"));
+    }
 }
