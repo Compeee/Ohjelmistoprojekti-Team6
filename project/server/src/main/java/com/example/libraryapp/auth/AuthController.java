d69a96c275fbcd3b354577d6d0c0e10bce6e14cb
Compeee <eero.kaarnalehto@outlook.com>
2022-11-28 16:58:27 +0200

    upd8

--- project/server/src/main/java/com/example/libraryapp/auth/AuthController.java
+++ project/server/src/main/java/com/example/libraryapp/auth/AuthController.java
@@ -3,8 +3,13 @@ package com.example.libraryapp.auth;
 import com.example.libraryapp.libraryUser.LibraryUser;
 import com.example.libraryapp.libraryUser.LibraryUserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,12 +22,16 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/api/v1/auth")
 public class AuthController {
+    @Autowired
+    private AuthenticationManager authenticationManager;
     private final LibraryUserService libraryUserService;
     @PostMapping("/authenticate")
-    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest loginRequest) throws Exception {
         Optional<LibraryUser> user = libraryUserService.validUsernameAndPassword(loginRequest.getUsername(), loginRequest.getPassword());
         if (user.isPresent()) {
             LibraryUser user1 = user.get();
+            Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
             return ResponseEntity.ok(new AuthResponse(user1.getId(), user1.getEmail(), user1.getRole()));
         }
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
